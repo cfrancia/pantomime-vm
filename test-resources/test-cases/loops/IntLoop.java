060772a932c32fcb3ee468834b8217a620d44a5c
cfrancia <cfrancia@users.noreply.github.com>
2016-10-02 21:47:46 +1100

    Reduce the number of iterations to prevent overflow

--- test-resources/test-cases/loops/IntLoop.java
+++ test-resources/test-cases/loops/IntLoop.java
@@ -6,7 +6,7 @@ public class IntLoop {
     public static void main(String[] args) {
         int total = 0;
 
-        for (int i = 0; i < 1_000_000; i++) {
+        for (int i = 0; i < 10; i++) {
             total += i;
         }
 
