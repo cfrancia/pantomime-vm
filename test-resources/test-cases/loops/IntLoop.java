8c820107b0bee489d3780e44592409d458d36a6c
cfrancia <cfrancia@users.noreply.github.com>
2016-10-02 16:14:05 +1100

    Added test case for int loops

---
+++ test-resources/test-cases/loops/IntLoop.java
@@ -0,0 +1,18 @@
+/**
+ * Created by cfrancia on 26/09/16.
+ */
+public class IntLoop {
+
+    public static void main(String[] args) {
+        int total = 0;
+
+        for (int i = 0; i < 1_000_000; i++) {
+            total += i;
+        }
+
+        println(total);
+    }
+
+    public static native void println(int val);
+
+}
